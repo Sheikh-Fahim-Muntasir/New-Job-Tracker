c7c75e04533a0e6c16eacf1ee08aff590ae56c1c
unknown <nirobniloy753@gmail.com  git config --global user.name>
2026-08-12 17:25:21 +0300

    total,interview and rejeted counter has functioned

--- machine.js
+++ machine.js
@@ -18,3 +18,21 @@ function Goto(id) {
     const showOnly = document.getElementById(id)
     showOnly.classList.remove("hidden")
 }
+`
+// job counter start`
+
+let totalStart = getValue("total-start");
+let interStart = getValue("total-inter");
+let rejectStart = getValue("total-reject");
+// total
+totalStart = document.getElementById("all-container").children.length;
+console.log("total jobs ", totalStart);
+document.getElementById("total-start").innerText = totalStart
+// interview total
+interStart = document.getElementById("inter-container").children.length;
+console.log("total interview ", interStart);
+document.getElementById("total-inter").innerText = interStart
+// rejected total
+rejectStart = document.getElementById("reject-container").children.length;
+console.log("total rejected ", rejectStart);
+document.getElementById("total-reject", rejectStart).innerText = rejectStart
